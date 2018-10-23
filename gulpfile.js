37a11655676679cc96447fac681a83fded201d4f
Koen Engels <koen.engels@zinderlabs.com>
2018-10-23 10:47:33 +0200

    inital commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const tsProject = require("gulp-typescript").createProject({
+  noImplicitAny: false
+});
+
+const jsWatch = "src/**/*.js";
+const tsWatch = "src/**/*.ts";
+
+gulp.task("babel", () => {
+  return gulp
+    .src(jsWatch)
+    .pipe(babel({ presets: ["@babel/preset-env"] }))
+    .pipe(gulp.dest("built/babelOut"));
+});
+
+gulp.task("ts", () => {
+  return gulp
+    .src(tsWatch)
+    .pipe(tsProject())
+    .pipe(gulp.dest("built/tsOut"));
+});
+
+gulp.task("default", ["babel", "ts"], () => {
+  gulp.watch(jsWatch, ["babel"]);
+  gulp.watch(tsWatch, ["ts"]);
+});
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+//write ts-code here
